271ddee7f62e81c52ec69ae7d39ea9957b056e5e
Alexandra Redfield <undeadlegendop@gmail.com>
2026-06-22 09:29:51 +0000

    Add files via upload

---
+++ hypr/modules/autostart.lua
@@ -0,0 +1,21 @@
+-------------------
+---- AUTOSTART ----
+-------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+--
+hl.on("hyprland.start", function () 
+  hl.exec_cmd("waybar")
+  hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+  hl.exec_cmd("awww-daemon")
+  hl.exec_cmd("snappy-switcher --daemon")
+  hl.exec_cmd("swaync")
+  hl.exec_cmd("hyprsunset")
+  hl.exec_cmd("swayosd-server")
+  hl.exec_cmd("~/.config/hypr/scripts/battery-check.sh")
+  hl.exec_cmd("~/.config/hypr/scripts/volume-notify.sh")
+  hl.exec_cmd("~/.config/hypr/scripts/brightness-notify.sh")
+end)
