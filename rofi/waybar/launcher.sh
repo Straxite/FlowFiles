c9e4d98ab23b4ecbff4197a6dad9b2a99507eca2
Inde Winters <undeadlegendop@gmail.com>
2026-06-24 08:31:54 +0000

    Add files via upload

---
+++ rofi/waybar/launcher.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+options="Notch\nVelvetline\nClassy\nDynamic-Notch\nBlayer\nLeftSider\nDynamic-Island\nVelvetlineV2"
+
+# Show rofi menu
+chosen=$(printf "$options" | rofi -dmenu \
+    -p "Select Layout" )
+
+# Exit if nothing selected
+[ -z "$chosen" ] && exit 0
+
+case "$chosen" in
+    Notch)
+        ~/.config/waybar/layout-scripts/notch.sh
+        ;;
+    Velvetline)
+        ~/.config/waybar/layout-scripts/velvetline.sh
+        ;;
+    Classy)
+        ~/.config/waybar/layout-scripts/classy.sh
+        ;;
+    Dynamic-Notch)
+        ~/.config/waybar/layout-scripts/dy-notch.sh
+        ;;
+    Blayer)
+        ~/.config/waybar/layout-scripts/blayer.sh
+        ;;
+    LeftSider)
+        ~/.config/waybar/layout-scripts/leftsider.sh
+        ;;
+    Dynamic-Island)
+    ~/.config/waybar/layout-scripts/dynamic-island.sh
+    ;;
+    VelvetlineV2)
+    ~/.config/waybar/layout-scripts/velvetlinev2.sh
+    ;;
+esac
