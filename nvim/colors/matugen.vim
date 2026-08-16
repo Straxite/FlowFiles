709612628a5795f3fad2311aa02aa323d53c0c72
Hannah Winters <undeadlegendop@gmail.com>
2026-08-16 15:14:07 +0000

    Add files via upload

--- nvim/colors/matugen.vim
+++ nvim/colors/matugen.vim
@@ -1,20 +1,20 @@
-hi Comment guibg=None guifg=#4d4446
+hi Comment guibg=None guifg=#444753
 
-hi Delimiter guibg=None guifg=#7b686a
-hi Operator guibg=None guifg=#7b686a
+hi Delimiter guibg=None guifg=#5f6474
+hi Operator guibg=None guifg=#5f6474
 
-hi Todo guibg=None guifg=#927a7c
+hi Todo guibg=None guifg=#6d7284
 
-hi Identifier guibg=None guifg=#cb2418
-hi Constant guibg=None guifg=#8d1312
-hi Type guibg=None guifg=#430d0d
-hi String guibg=None guifg=#a88c8d
-hi Special guibg=None guifg=#090f16
-hi PreProc guibg=None guifg=#090f16
-hi Function guibg=None guifg=#101c1c
-hi Statement guibg=None guifg=#51454b
+hi Identifier guibg=None guifg=#595239
+hi Constant guibg=None guifg=#484f68
+hi Type guibg=None guifg=#353b51
+hi String guibg=None guifg=#7a7f94
+hi Special guibg=None guifg=#25293d
+hi PreProc guibg=None guifg=#25293d
+hi Function guibg=None guifg=#292e40
+hi Statement guibg=None guifg=#213234
 
 hi Error guibg=#93000a guifg=#ffdad6
-hi StatusLine guibg=#ffb4a7 guifg=#561e16
-hi StatusLineNC guibg=#73342a guifg=#ffdad4
-hi Selection guibg=#363234
+hi StatusLine guibg=#adc6ff guifg=#112f60
+hi StatusLineNC guibg=#2b4678 guifg=#d8e2ff
+hi Selection guibg=#373943
--- nvim/init.lua
+++ nvim/init.lua
@@ -17,10 +17,40 @@ require("lazy").setup("plugins", {
        timeout = 600, -- seconds, default is much lower
      },
    })
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+local colors = {
+  blue   = '#80a0ff',
+  cyan   = '#79dac8',
+  black  = '#080808',
+  white  = '#c6c6c6',
+  red    = '#ff5189',
+  violet = '#d183e8',
+  grey   = '#303030',
+}
+
+local bubble_theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.violet },
+    b = { fg = colors.white, bg = colors.grey },
+    c = { fg = colors.white },
+  },
+
+  insert = { a = { fg = colors.black, bg = colors.blue } },
+  visual = { a = { fg = colors.black, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.white, bg = colors.black },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white },
+  },
+}
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'auto',
+    theme = bubbles_theme,
     component_separators = { left = '', right = '│'},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
@@ -50,18 +80,18 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_a = {{ 'mode', fmt = function(str) return '  ' .. str end }},
+    lualine_b = {{ 'filename', fmt = function(str) return ' ' .. str end }, 'branch', 'diff', 'diagnostics'},
+    lualine_c = {},
     lualine_x = {},
-    lualine_y = {},
-    lualine_z = {'location'}
+    lualine_y = {{ 'progress', fmt = function(str) return ' ' .. str end }},
+    lualine_z = {{ 'location', fmt = function(str) return ' ' .. str end }}
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = {},
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {}
   },
