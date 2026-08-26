ccdff54a1c263405bf842803eeca8394d517a880
Hannah Winters <undeadlegendop@gmail.com>
2026-08-26 19:52:41 +0530

    Update refresher.sh

--- install-script/refresher.sh
+++ install-script/refresher.sh
@@ -10,20 +10,4 @@ BLUE='\033[0;34m'
 BOLD='\033[1m'
 RESET='\033[0m'
 
-# ── Refresh ───────────────────────────────────────────────────────────────
-
-echo -e "${GREEN} reloading mkinitcpio.conf"
-
-sudo mkinitcpio -P
-
-echo -e "${GREEN} reloading grub"
-
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-
-sudo echo "splash" | sudo tee /etc/kernel/cmdline
-
-echo -e "${GREEN} reloading grub once again cuz the script writer, which is me is a dummass"
-
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-
-sudo plymouth-set-default-theme -R flow
+# ── Refresh ───────────────────────────────────────────────────────────────────
